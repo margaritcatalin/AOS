b3dd7dcc0116b997b10b6e4bbfa4d69dfa64856d
Mărgărit Marian Cătălin <margarit.catalin97@gmail.com>
2018-11-11 16:28:27 +0200

    Tema 2

---
+++ tema2/MySQL/init.sql
@@ -0,0 +1,9 @@
+use UsersData;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);
